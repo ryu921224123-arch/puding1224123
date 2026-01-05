91ba9f1f394984e2a40feffb1c83538993f51863
ryu921224123-arch <ryu921224123@gmail.com>
2026-01-05 21:12:42 +0900

    Update directions.js

--- api/directions.js
+++ api/directions.js
@@ -1,15 +1,25 @@
 export default async function handler(req, res) {
   const { start, goal } = req.query;
 
-  const response = await fetch(
-    `https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start=${start}&goal=${goal}&option=traoptimal`,
-    {
-      headers: {
-        'X-NCP-APIGW-API-KEY-ID': process.env.NAVER_MAP_CLIENT_ID,
-        'X-NCP-APIGW-API-KEY': process.env.NAVER_MAP_CLIENT_SECRET,
-      },
-    }
-  );
+  console.log('start:', start);
+  console.log('goal:', goal);
+
+  if (!start || !goal) {
+    return res.status(400).json({ error: 'start or goal missing' });
+  }
+
+  const url =
+    `https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving` +
+    `?start=${encodeURIComponent(start)}` +
+    `&goal=${encodeURIComponent(goal)}` +
+    `&option=traoptimal`;
+
+  const response = await fetch(url, {
+    headers: {
+      'X-NCP-APIGW-API-KEY-ID': process.env.NAVER_MAP_CLIENT_ID,
+      'X-NCP-APIGW-API-KEY': process.env.NAVER_MAP_CLIENT_SECRET,
+    },
+  });
 
   const data = await response.json();
   res.status(200).json(data);
