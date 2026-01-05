e76931bd5a528b8ea353e881b65e499d39b035e3
ryu921224123-arch <ryu921224123@gmail.com>
2026-01-05 21:10:44 +0900

    Update directions.js

--- api/directions.js
+++ api/directions.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const { start, goal } = req.query;
 
   const response = await fetch(
-    `https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start=${start}&goal=${goal}`,
+    `https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start=${start}&goal=${goal}&option=traoptimal`,
     {
       headers: {
         'X-NCP-APIGW-API-KEY-ID': process.env.NAVER_MAP_CLIENT_ID,
@@ -12,7 +12,5 @@ export default async function handler(req, res) {
   );
 
   const data = await response.json();
-
-  // 🔥 전체 응답 그대로 반환
   res.status(200).json(data);
 }
